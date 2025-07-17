a59f2ae2dfea85e31e4fccd2a3a34507b7d9fe9e
iSmo0ke <jesus.alt.2004@gmail.com>
2025-07-17 07:22:13 -0600

    Enviar datos - error

--- src/api/perfil.js
+++ src/api/perfil.js
@@ -1,23 +1,32 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+//consultar clientes registrados
 export function useClientes() {
     const [clientes, setClientes] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        axios.get("http://localhost:3307/api/clientes/conClientes")
-            .then((response) => {
-                setClientes(response.data);
-                setLoading(false);
-            })
-            .catch((err) => {
-                console.error("Error al obtener clientes:", err);
-                setError(err);
-                setLoading(false);
-            });
+        const token = localStorage.getItem("token");
+
+        axios.get("http://localhost:3307/api/clientes/conClientes", {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        })
+        .then((response) => {
+            setClientes(response.data);
+            setLoading(false);
+        })
+        .catch((err) => {
+            console.error("Error al obtener clientes:", err);
+            setError(err);
+            setLoading(false);
+        });
     }, []);
 
     return { clientes, loading, error };
 }
+
+//Registrar datos registrados de usuario 
